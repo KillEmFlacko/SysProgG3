fea726fd4c4b5c88546eb51bcbd94d81cc05ae15
Armando Falanga <armando.falanga@gmail.com>
2021-07-15 10:11:08 +0200

    [CAIPC01] creation of test modules

---
+++ test/CAIPC01_sem/main.c
@@ -0,0 +1,52 @@
+/*
+ * Course: System Programming 2020/2021
+ *
+ * Lecturers:
+ * Alessia		Saggese asaggese@unisa.it
+ * Francesco	Moscato	fmoscato@unisa.it
+ *
+ * Group:
+ * D'Alessio	Simone
+ * Falanga		Armando		0622701140  a.falanga13@studenti.unisa.it
+ * Fattore		Alessandro
+ *
+ * Copyright (C) 2021 - All Rights Reserved
+ *
+ * This file is part of SysProgG3.
+ *
+ * SysProgG3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SysProgG3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SysProgG3.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <libgen.h>
+#include "lib/lib.h"
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define FAILURE_MESSAGE ANSI_COLOR_RED"Test FAILED"ANSI_COLOR_RESET
+
+int main(int argc, char **argv){
+	assert(add(1,2) == 3);
+	printf("[%s] "ANSI_COLOR_GREEN"Test OK\n"ANSI_COLOR_RESET,basename(argv[0]));
+	exit(EXIT_SUCCESS);
+}
+
