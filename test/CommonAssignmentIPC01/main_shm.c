70df54fd358b629b03cc549766dbb5b0ffe743f4
Armando Falanga <armando.falanga@gmail.com>
2021-07-19 17:44:36 +0200

    [CAIPC01] multi exec makefile

--- test/CommonAssignmentIPC01/main_shm.c
+++ test/CommonAssignmentIPC01/main_shm.c
@@ -35,8 +35,6 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/ipc.h>
-#include "lib/lib.h"
-#include "CommonAssignmentIPC01/libsp.h"
 
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
