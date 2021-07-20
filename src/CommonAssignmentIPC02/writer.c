07df9247d8f74789a8de1faeef252fad026d14e9
sim1e097cd <dalessiosimone1@gmail.com>
2021-07-20 12:12:37 +0200

    [CAIPC02] Complex solution added, but maybe good for concurrency

--- src/CommonAssignmentIPC02/writer.c
+++ src/CommonAssignmentIPC02/writer.c
@@ -7,49 +7,161 @@
 #define S_WRITE 0
 #define S_NUM_WRITERS 1
 #define S_NUM_READERS 2
+#define S_BATCH_READERS 3
 
-void write(Monitor *mon, int *val, int *nr, int *nw, int new_val)
+// A linked list (LL) node to store a queue entry
+struct QNode {
+    int key;
+    struct QNode* next;
+};
+  
+// The queue, front stores the front node of LL and rear stores the
+// last node of LL
+struct Queue {
+    struct QNode *front, *rear;
+};
+  
+// A utility function to create a new linked list node.
+struct QNode* newNode(int k)
 {
+    struct QNode* temp = (struct QNode*)malloc(sizeof(struct QNode));
+    temp->key = k;
+    temp->next = NULL;
+    return temp;
+}
+  
+// A utility function to create an empty queue
+struct Queue* createQueue()
+{
+    struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue));
+    q->front = q->rear = NULL;
+    return q;
+}
+  
+// The function to add a key k to q
+void enQueue(struct Queue* q, int k)
+{
+    // Create a new LL node
+    struct QNode* temp = newNode(k);
+  
+    // If queue is empty, then new node is front and rear both
+    if (q->rear == NULL) {
+        q->front = q->rear = temp;
+        return;
+    }
+  
+    // Add the new node at the end of queue and change rear
+    q->rear->next = temp;
+    q->rear = temp;
+}
+  
+// Function to remove a key from given queue q
+int deQueue(struct Queue* q)
+{
+    // If queue is empty, return NULL.
+    if (q->front == NULL)
+        return;
+  
+    // Store previous front and move front one node ahead
+    struct QNode* temp = q->front;
+
+    int key = temp->key;
+  
+    q->front = q->front->next;
+  
+    // If front becomes NULL, then change rear also as NULL
+    if (q->front == NULL)
+        q->rear = NULL;
+  
+    free(temp);
+
+    return key;
+}
+
+void write(Monitor *mon, struct Queue *q, int *val, int *nr, int *nw, int new_val)
+{
+    int n_readers, n_writers;
+
     enter_monitor(mon);
 
     // Mi metto in coda per la scrittura
     wait_cond(mon, S_NUM_WRITERS);
     *nw++;
-    // Verifico quanti processi lettori ci sono
+    signal_cond(mon, S_NUM_WRITERS);
+    
+    // Azzero la coda di lettori in attesa
     wait_cond(mon, S_NUM_READERS);
-    // Se ci sono aspetto in coda
-    if (*nr > 0)
-    {
-        wait_cond(mon, S_WRITE);
-    }
+    // Salvo il blocco di lettori precedenti in attesa
+    enQueue(q, *nr);
+    *nr = 0;
     signal_cond(mon, S_NUM_READERS);
-    // Se non ci sono scrivo
+    // Il prossimo lettore ad entrare si vedrà come primo
+
+    // Tento la scrittura
     wait_cond(mon, S_WRITE);
     // Affinchè scriva non devono esserci neanche altri scrittori
     *val = new_val;
-    *nw--;
     signal_cond(mon, S_WRITE);
+    
+
+    // Decremento il numero di scrittori
+    wait_cond(mon, S_NUM_WRITERS);
+    *nw--;
+    signal_cond(mon, S_NUM_WRITERS);
     
     leave_monitor(mon);
 }
 
-void read(Monitor *mon, int *val, int *nr, int *nw)
+void read(Monitor *mon, struct Queue *q, int *val, int *nr, int *nw)
 {
+    int n_readers, n_writers;
+
     enter_monitor(mon);
 
     // Mi metto in coda per la lettura
     wait_cond(mon, S_NUM_READERS);
-    *nr++;
+    n_readers = *nr++;
+    signal_cond(mon, S_NUM_READERS);
+
     // Verifico quanti processi scrittori ci sono
     wait_cond(mon, S_NUM_WRITERS);
-    // Se ci sono aspetto in coda
-    if (*nw > 0)
-    {
-        wait_cond(mon, S_WRITE);
-    }
+    n_writers = *nw;
     signal_cond(mon, S_NUM_WRITERS);
+
+    // Se ci sono aspetto in coda
+    if (n_writers > 0)
+    {
+        // Il primo lettore prende il lock della scrittura e crea un semaforo a cui
+        // si accodano gli altri lettori che vengono dopo di lui
+        if (n_readers == 1)
+        {
+            
+            wait_cond(mon, S_WRITE);
+
+            // Appena prendo il lock di scrittura (quindi blocco altri scrittori)
+            // vedo quanti lettori ho accodati da sbloccare
+            int batch = deQueue(q);
+
+            // Quando il primo lettore legge deve sbloccare tutti gli altri lettori
+            // accodati dietro di lui
+            for (int i=0; i<batch; i++)
+            {
+                signal_cond(mon, S_BATCH_READERS);
+            }
+            
+        }
+        else
+        {
+            wait_cond(mon, S_BATCH_READERS);
+        }
+        
+        // Una volta sbloccato il primo lettore anche quelli dietro di esso devono procedere
+    }
+    
     // Se non ci sono stampo
     printf("VALUE: %d", *val);
+
+    signal_wait(mon, S_NUM_READERS);
     *nr--;
     // L'ultimo lettore consente allo scrittore di procedere
     if (*nr == 0)
@@ -69,6 +181,8 @@ int main(int argc, char **argv)
 	int value = 0;
     int n_writers = 0;
     int n_readers = 0;
+
+    struct Queue* q = createQueue();
 
     // Generating key for shared memory
 	if ((key = ftok(".", 100)) == -1) { perror("ftok"); exit(1); }
