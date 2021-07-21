22d8afd8f8bb12061774230a6e8bc9daaf483c49
sim1e097cd <dalessiosimone1@gmail.com>
2021-07-21 21:29:49 +0200

    [CAIPC02] Added final solution (to substitute monitor with 4 separated semaphores)

--- src/CommonAssignmentIPC02/readwrite_sem.c
+++ src/CommonAssignmentIPC02/readwrite_sem.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "CommonAssignmentIPC01/libsp.h"
-#include "lib/lib.h"
+#include "libsp.h"
 
 #define NCOND 4
 #define S_WRITE 0
@@ -10,78 +9,41 @@
 #define S_NUM_READERS 2
 #define S_BATCH_READERS 3
 
-// A linked list (LL) node to store a queue entry
-struct QNode {
-    int key;
-    struct QNode* next;
-};
-  
-// The queue, front stores the front node of LL and rear stores the
-// last node of LL
-struct Queue {
-    struct QNode *front, *rear;
-};
-  
-// A utility function to create a new linked list node.
-struct QNode* newNode(int k)
-{
-    struct QNode* temp = (struct QNode*)malloc(sizeof(struct QNode));
-    temp->key = k;
-    temp->next = NULL;
-    return temp;
-}
-  
-// A utility function to create an empty queue
-struct Queue* createQueue()
-{
-    struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue));
-    q->front = q->rear = NULL;
-    return q;
-}
-  
-// The function to add a key k to q
-void enQueue(struct Queue* q, int k)
-{
-    // Create a new LL node
-    struct QNode* temp = newNode(k);
-  
-    // If queue is empty, then new node is front and rear both
-    if (q->rear == NULL) {
-        q->front = q->rear = temp;
-        return;
-    }
-  
-    // Add the new node at the end of queue and change rear
-    q->rear->next = temp;
-    q->rear = temp;
-}
-  
-// Function to remove a key from given queue q
-int deQueue(struct Queue* q)
-{
-    // If queue is empty, return NULL.
-    if (q->front == NULL)
-        return NULL;
-  
-    // Store previous front and move front one node ahead
-    struct QNode* temp = q->front;
+#define BUFFER_SIZE 100
 
-    int key = temp->key;
-  
-    q->front = q->front->next;
-  
-    // If front becomes NULL, then change rear also as NULL
-    if (q->front == NULL)
-        q->rear = NULL;
-  
-    free(temp);
+typedef struct {
+    int batch[BUFFER_SIZE];
+    int front;
+    int back;
+    int num_elements;
+} BatchReaders;
 
-    return key;
+BatchReaders* initBatchReaders()
+{
+    BatchReaders* b = (BatchReaders*)malloc(sizeof(BatchReaders));
+    b -> front = b -> back = 0;
+    b -> num_elements = 0;
+    return b;
 }
 
-void writer(Monitor *mon, struct Queue *q, int *val, int *nr, int *nw, int new_val)
+int push(BatchReaders* b, int val)
 {
-    int n_readers, n_writers;
+    if((++(b -> num_elements)) == BUFFER_SIZE) return -1;
+    b -> batch [b -> front] = val;
+    b -> front = (b -> front + 1) % BUFFER_SIZE;
+    return 0;
+}
+
+int pop(BatchReaders* b)
+{
+    if (b -> num_elements == 0) return 0;
+    int returnValue = b -> batch [b -> back];
+    b -> back = (b -> back + 1) % BUFFER_SIZE;
+    return returnValue;
+}
+
+void writer(Monitor *mon, BatchReaders *b, int *val, int *nr, int *nw, int new_val)
+{
 
     //enter_monitor(mon);
     printf("### INIZIO PROCESSO SCRITTURA ###\n");
@@ -92,10 +54,10 @@ void writer(Monitor *mon, struct Queue *q, int *val, int *nr, int *nw, int new_v
     printf("n_writers: %d\n", *nw);
     signal_sem(mon -> id_cond, S_NUM_WRITERS, 0);
     
-    printf("Azzero la coda di lettori in attesa\n");
     wait_sem(mon -> id_cond, S_NUM_READERS, 0);
-    printf("Salvo il blocco di lettori precedenti in attesa\n");
-    enQueue(q, *nr);
+    printf("Salvo il blocco di lettori precedenti in attesa che è %d\n",*nr);
+    push(b, *nr);
+    printf("Azzero la coda di lettori in attesa\n");
     *nr = 0;
     signal_sem(mon -> id_cond, S_NUM_READERS, 0);
     printf("Il prossimo lettore ad entrare si vedrà come primo\n");
@@ -104,6 +66,9 @@ void writer(Monitor *mon, struct Queue *q, int *val, int *nr, int *nw, int new_v
     wait_sem(mon -> id_cond, S_WRITE, 0);
     // Affinchè scriva non devono esserci neanche altri scrittori
     printf("Effettuo la scrittura\n");
+    // ***
+    sleep(12);
+    // ***
     *val = new_val;
     signal_sem(mon -> id_cond, S_WRITE, 0);
     
@@ -117,63 +82,78 @@ void writer(Monitor *mon, struct Queue *q, int *val, int *nr, int *nw, int new_v
     printf("### FINE PROCESSO SCRITTURA ###\n\n");
 }
 
-void reader(Monitor *mon, struct Queue *q, int *val, int *nr, int *nw)
+void reader(Monitor *mon, BatchReaders *b, int *val, int *nr, int *nw)
 {
-    int n_readers, n_writers;
+    int batch, n_readers, n_writers;
 
     //enter_monitor(mon);
     printf("### INIZIO PROCESSO LETTURA ###\n");
 
     printf("Mi metto in coda per la lettura\n");
     wait_sem(mon -> id_cond, S_NUM_READERS, 0);
-    n_readers = *nr++;
+    n_readers = ++(*nr);
     printf("n_readers: %d\n", n_readers);
     signal_sem(mon -> id_cond, S_NUM_READERS, 0);
- 
-    printf("Verifico quanti processi scrittori ci sono\n");
-    wait_sem(mon -> id_cond, S_NUM_WRITERS, 0);
-    n_writers = *nw;
-    printf("n_writers: %d\n", n_writers);
-    signal_sem(mon -> id_cond, S_NUM_WRITERS, 0);
-    
-    if (n_writers > 0)
+
+
+    printf("Se ci sono scrittori aspetto in coda...\n");
+    // Il primo lettore prende il lock della scrittura
+    if (n_readers == 1)
     {
-        printf("Se ci sono aspetto in coda\n");
-        // Il primo lettore prende il lock della scrittura
-        if (n_readers == 1)
+        printf("Sono il primo lettore\n");
+        wait_sem(mon -> id_cond, S_WRITE, 0);
+
+        // Appena prendo il lock di scrittura (quindi blocco altri scrittori)
+        // vedo quanti lettori ho accodati da sbloccare
+
+        printf("Verifico quanti processi scrittori ci sono\n");
+        wait_sem(mon -> id_cond, S_NUM_WRITERS, 0);
+        n_writers = *nw;
+        printf("n_writers: %d\n", n_writers);
+        signal_sem(mon -> id_cond, S_NUM_WRITERS, 0);
+
+        if (n_writers > 0)
         {
-            
-            wait_sem(mon -> id_cond, S_WRITE, 0);
-
-            // Appena prendo il lock di scrittura (quindi blocco altri scrittori)
-            // vedo quanti lettori ho accodati da sbloccare
-            int batch = deQueue(q);
-
-            // Quando il primo lettore legge deve sbloccare tutti gli altri lettori
-            // accodati dietro di lui
-            for (int i=0; i<batch; i++)
-            {
-                signal_sem(mon -> id_cond, S_BATCH_READERS, 0);
-            }
-            
+            // Se ci sono processi scrittori il primo ha definito un batch
+            batch = pop(b);
         }
         else
         {
-            wait_sem(mon -> id_cond, S_BATCH_READERS, 0);
+            // Non ci sono processi scrittori quindi sblocco tutti i lettori escluso me
+            wait_sem(mon -> id_cond, S_NUM_READERS, 0);
+            batch = *nr - 1;
+            printf("n_readers: %d\n", n_readers);
+            signal_sem(mon -> id_cond, S_NUM_READERS, 0);
         }
         
-        // Una volta sbloccato il primo lettore anche quelli dietro di esso devono procedere
+        printf("Numero processi accodati: %d\n",batch);
+
+        // Quando il primo lettore legge deve sbloccare tutti gli altri lettori
+        // accodati dietro di lui
+        for (int i=0; i<batch; i++)
+        {
+            signal_sem(mon -> id_cond, S_BATCH_READERS, 0);
+        }
+        
     }
+    else
+    {
+        printf("Sono un lettore che si accoda\n");
+        wait_sem(mon -> id_cond, S_BATCH_READERS, 0);
+        printf("Sono stato sbloccato da un lettore\n");
+    }
+    
+    // Una volta sbloccato il primo lettore anche quelli dietro di esso devono procedere
     
     // Se non ci sono stampo
     printf("VALUE: %d\n", *val);
 
     wait_sem(mon -> id_cond, S_NUM_READERS, 0);
-    *nr--;
     // L'ultimo lettore consente allo scrittore di procedere
-    if (*nr == 0)
+    if (--(*nr) == 0)
     {
         signal_sem(mon -> id_cond, S_WRITE, 0);
+        printf("Sono l'ultimo lettore del gruppo che rilascia il semaforo per la scrittura\n");
     }
     signal_sem(mon -> id_cond, S_NUM_READERS, 0);
 
@@ -192,7 +172,7 @@ int main(int argc, char **argv)
 	int *value = 0;
     int *n_writers = 0;
     int *n_readers = 0;
-    struct Queue* q = createQueue();
+    BatchReaders* b = initBatchReaders();
     /*
     ************************************
     */
@@ -211,7 +191,8 @@ int main(int argc, char **argv)
     get_shm(&key0, &value, sizeof(int));
     get_shm(&key1, &n_writers, sizeof(int));
     get_shm(&key2, &n_readers, sizeof(int));
-    get_shm(&key3, q, sizeof(struct Queue));
+    printf("Adesso provo a creare la coda in shm...\n");
+    get_shm(&key3, b, sizeof(BatchReaders));
 
     // Reset value in case of existing shm
     *value = 0;
@@ -235,20 +216,35 @@ int main(int argc, char **argv)
 
     if (fork() == 0)
     {
-        sleep(20);
         fprintf(stdout,"SONS START\n");
-        sleep(1);
-        reader(mon, q, value, n_readers, n_writers);
-        sleep(10);
-        reader(mon, q, value, n_readers, n_writers);
-        return 0;
+
+        if (fork() == 0)
+        {
+            while (1) 
+            {
+                sleep(1);
+                reader(mon, b, value, n_readers, n_writers);
+            }
+        }
+        else
+        {
+            while (1) 
+            {
+                sleep(1);
+                reader(mon, b, value, n_readers, n_writers);
+            }
+        }
+        
     }
     else
     {
-        num++;
-        sleep(5);
         fprintf(stdout,"FATHER START\n");
-        writer(mon, q, value, n_readers, n_writers, num);
+        while (1) 
+        {
+            num++;
+            sleep(3);
+            writer(mon, b, value, n_readers, n_writers, num);
+        }
     }
     
     return 0;
