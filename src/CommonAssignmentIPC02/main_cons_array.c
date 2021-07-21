fac89a46fb1df7d560ae7b1e0c80dd7921f38bb8
Armando Falanga <armando.falanga@gmail.com>
2021-07-21 17:55:18 +0200

    [CAIPC02] starting array version ov 1R & 1W

---
+++ src/CommonAssignmentIPC02/main_cons_array.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "CommonAssignmentIPC01/libsp.h"
+
+#define NOTFULL_SEM 1
+#define NOTEMPTY_SEM 0
+
+int id_sem = -1, id_shared = -1;
+
+void exit_procedure(void)
+{
+	remove_shm(id_shared);
+	remove_sem(id_sem);
+}
+
+/*
+ * One producer and one consumer, one variable in shared memory
+ */
+int main(int argc, char **argv)
+{
+	key_t key = ftok(KEY_FILE,1);
+	int* shm_addr;
+
+	atexit(exit_procedure);
+
+	/*
+	 * Create semaphore set with NOTFULL and NOTEMPTY
+	 */
+	if((id_sem = get_sem(&key,2,0)) == -1)
+	{
+		fprintf(stderr,"Cannot get semaphore\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/*
+	 * Create and attach shared memory area
+	 */
+	if((id_shared = get_shm(&key,(char**)&shm_addr,sizeof(int))) == -1)
+	{
+		fprintf(stderr,"Cannot get shared memory area\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/*
+	 * Wait for user input to produce
+	 */
+	char c;
+	srand(time(NULL));
+
+	printf("Press a key to consume (Ctrl-D to exit)");
+	while((c = getchar()) != EOF)
+	{
+		if(c != '\n') putchar('\n');
+
+		/*
+		 * Wait for content to be avaliable and buffer to be full
+		 */
+		if(wait_sem(id_sem,NOTEMPTY_SEM,0) == -1)
+		{
+			fprintf(stderr,"Error while waiting full buffer\n");
+			exit(EXIT_FAILURE);
+		}
+
+		printf("Consumed value: %d\n",*shm_addr);
+
+		/*
+		 * Signal to producer that buffer is empty
+		 */
+		if(signal_sem(id_sem,NOTFULL_SEM,0) == -1)
+		{
+			fprintf(stderr,"Error while signaling empty buffer\n");
+			exit(EXIT_FAILURE);
+		}
+		printf("Press a key to consume (Ctrl-D to exit)");
+	}
+
+	exit(EXIT_SUCCESS);
+}
---
+++ src/CommonAssignmentIPC02/main_prod_array.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "CommonAssignmentIPC01/libsp.h"
+
+int id_sem_notfull = -1, id_sem_notempty = -1, id_shared = -1;
+
+void exit_procedure(void)
+{
+	if(id_shared != -1) remove_shm(id_shared);
+	if(id_sem_notfull != -1) remove_sem(id_sem_notfull);
+	if(id_sem_notempty != -1) remove_sem(id_sem_notempty);
+}
+
+/*
+ * One producer and one consumer, one variable in shared memory
+ */
+int main(int argc, char **argv)
+{
+	atexit(exit_procedure);
+
+	if(argc < 2)
+	{
+		fprintf(stderr,"Usage:\t%s num_res\n",argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	int nres = atoi(argv[1]);
+	
+	key_t key_shm = ftok(KEY_FILE,1);
+	key_t key_notfull = ftok(KEY_FILE,2);
+	key_t key_notempty = ftok(KEY_FILE,3);
+
+	/*
+	 * Create semaphore set for NOTFULL
+	 */
+	if((id_sem_notfull = get_sem(&key_notfull,nres,1)) == -1)
+	{
+		fprintf(stderr,"Cannot get notfull semaphore\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	/*
+	 * Create semaphore set for NOTEMPTY
+	 */
+	if((id_sem_notempty = get_sem(&key_notempty,nres,0)) == -1)
+	{
+		fprintf(stderr,"Cannot get notempty semaphore\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/*
+	 * Create and attach shared memory area
+	 */
+	int* shm_addr;
+	if((id_shared = get_shm(&key_shm,(char**)&shm_addr,sizeof(int)*nres)) == -1)
+	{
+		fprintf(stderr,"Cannot get shared memory area\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/*
+	 * Wait for user input to produce
+	 */
+	char c;
+	int res;
+	srand(time(NULL));
+	printf("Press a key to produce (Ctrl-D to exit)");
+	while((c = getchar()) != EOF)
+	{
+		if(c != '\n') putchar('\n');
+
+		printf("Enter resource to use:");
+		scanf("%d",&res);
+		
+		/*
+		 * Wait for content to be consumed and buffer to be empty
+		 */
+		if(wait_sem(id_sem_notfull,res,0) == -1)
+		{
+			fprintf(stderr,"Error while waiting empty buffer\n");
+			exit(EXIT_FAILURE);
+		}
+
+		shm_addr[res] = rand();
+		printf("Produced value: %d\n",shm_addr[res]);
+
+		/*
+		 * Signal to consumer that there is something in the buffer
+		 */
+		if(signal_sem(id_sem_notempty,res,0) == -1)
+		{
+			fprintf(stderr,"Error while signaling full buffer\n");
+			exit(EXIT_FAILURE);
+		}
+		printf("Press a key to produce (Ctrl-D to exit)");
+	}
+
+	exit(EXIT_SUCCESS);
+}
