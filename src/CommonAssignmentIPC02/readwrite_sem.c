bd0eb615c42daa86f7ea8afa8bd57a1635df34d8
sim1e097cd <dalessiosimone1@gmail.com>
2021-07-22 11:07:48 +0200

    [CAIPC02] Removed old main

--- src/CommonAssignmentIPC02/readwrite_sem.c
+++ src/CommonAssignmentIPC02/readwrite_sem.c
@@ -1,9 +1,7 @@
-#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "CommonAssignmentIPC01/libsp.h"
-#include "lib/lib.h"
+#include "libsp.h"
 
 #define NCOND 4
 #define S_WRITE 0
@@ -20,10 +18,11 @@ typedef struct {
     int num_elements;
 } BatchReaders;
 
-void initBatchReaders(BatchReaders * b)
+BatchReaders* initBatchReaders(BatchReaders* b)
 {
     b -> front = b -> back = 0;
     b -> num_elements = 0;
+    return b;
 }
 
 int push(BatchReaders* b, int val)
@@ -42,58 +41,54 @@ int pop(BatchReaders* b)
     return returnValue;
 }
 
-void writer(Monitor *mon, BatchReaders *b, int *val, int *nr, int *nw, int new_val)
+void writer(int sem, BatchReaders *b, int *val, int *nr, int *nw, int new_val)
 {
-
-    //enter_monitor(mon);
-    printf("### INIZIO PROCESSO SCRITTURA ###\n");
+    printf("\n### INIZIO PROCESSO SCRITTURA ###\n");
 
     printf("Mi metto in coda per la scrittura\n");
-    wait_sem(mon -> id_cond, S_NUM_WRITERS, 0);
+    wait_sem(sem, S_NUM_WRITERS, 0);
     (*nw)++;
     printf("n_writers: %d\n", *nw);
-    signal_sem(mon -> id_cond, S_NUM_WRITERS, 0);
+    signal_sem(sem, S_NUM_WRITERS, 0);
     
-    wait_sem(mon -> id_cond, S_NUM_READERS, 0);
+    wait_sem(sem, S_NUM_READERS, 0);
     printf("Salvo il blocco di lettori precedenti in attesa che è %d\n",*nr);
     push(b, *nr);
     printf("Azzero la coda di lettori in attesa\n");
     *nr = 0;
-    signal_sem(mon -> id_cond, S_NUM_READERS, 0);
+    signal_sem(sem, S_NUM_READERS, 0);
     printf("Il prossimo lettore ad entrare si vedrà come primo\n");
 
     // Tento la scrittura
-    wait_sem(mon -> id_cond, S_WRITE, 0);
+    wait_sem(sem, S_WRITE, 0);
     // Affinchè scriva non devono esserci neanche altri scrittori
     printf("Effettuo la scrittura\n");
     // ***
     sleep(12);
     // ***
     *val = new_val;
-    signal_sem(mon -> id_cond, S_WRITE, 0);
+    signal_sem(sem, S_WRITE, 0);
     
 
     printf("Decremento il numero di scrittori\n");
-    wait_sem(mon -> id_cond, S_NUM_WRITERS, 0);
+    wait_sem(sem, S_NUM_WRITERS, 0);
     (*nw)--;
-    signal_sem(mon -> id_cond, S_NUM_WRITERS, 0);
+    signal_sem(sem, S_NUM_WRITERS, 0);
     
-    //leave_monitor(mon);
     printf("### FINE PROCESSO SCRITTURA ###\n\n");
 }
 
-void reader(Monitor *mon, BatchReaders *b, int *val, int *nr, int *nw)
+void reader(int sem, BatchReaders *b, int *val, int *nr, int *nw)
 {
     int batch, n_readers, n_writers;
 
-    //enter_monitor(mon);
-    printf("### INIZIO PROCESSO LETTURA ###\n");
+    printf("\n### INIZIO PROCESSO LETTURA ###\n");
 
     printf("Mi metto in coda per la lettura\n");
-    wait_sem(mon -> id_cond, S_NUM_READERS, 0);
+    wait_sem(sem, S_NUM_READERS, 0);
     n_readers = ++(*nr);
     printf("n_readers: %d\n", n_readers);
-    signal_sem(mon -> id_cond, S_NUM_READERS, 0);
+    signal_sem(sem, S_NUM_READERS, 0);
 
 
     printf("Se ci sono scrittori aspetto in coda...\n");
@@ -101,16 +96,16 @@ void reader(Monitor *mon, BatchReaders *b, int *val, int *nr, int *nw)
     if (n_readers == 1)
     {
         printf("Sono il primo lettore\n");
-        wait_sem(mon -> id_cond, S_WRITE, 0);
+        wait_sem(sem, S_WRITE, 0);
 
         // Appena prendo il lock di scrittura (quindi blocco altri scrittori)
         // vedo quanti lettori ho accodati da sbloccare
 
         printf("Verifico quanti processi scrittori ci sono\n");
-        wait_sem(mon -> id_cond, S_NUM_WRITERS, 0);
+        wait_sem(sem, S_NUM_WRITERS, 0);
         n_writers = *nw;
         printf("n_writers: %d\n", n_writers);
-        signal_sem(mon -> id_cond, S_NUM_WRITERS, 0);
+        signal_sem(sem, S_NUM_WRITERS, 0);
 
         if (n_writers > 0)
         {
@@ -120,10 +115,10 @@ void reader(Monitor *mon, BatchReaders *b, int *val, int *nr, int *nw)
         else
         {
             // Non ci sono processi scrittori quindi sblocco tutti i lettori escluso me
-            wait_sem(mon -> id_cond, S_NUM_READERS, 0);
+            wait_sem(sem, S_NUM_READERS, 0);
             batch = *nr - 1;
             printf("n_readers: %d\n", n_readers);
-            signal_sem(mon -> id_cond, S_NUM_READERS, 0);
+            signal_sem(sem, S_NUM_READERS, 0);
         }
         
         printf("Numero processi accodati: %d\n",batch);
@@ -132,14 +127,14 @@ void reader(Monitor *mon, BatchReaders *b, int *val, int *nr, int *nw)
         // accodati dietro di lui
         for (int i=0; i<batch; i++)
         {
-            signal_sem(mon -> id_cond, S_BATCH_READERS, 0);
+            signal_sem(sem, S_BATCH_READERS, 0);
         }
         
     }
     else
     {
         printf("Sono un lettore che si accoda\n");
-        wait_sem(mon -> id_cond, S_BATCH_READERS, 0);
+        wait_sem(sem, S_BATCH_READERS, 0);
         printf("Sono stato sbloccato da un lettore\n");
     }
     
@@ -148,30 +143,29 @@ void reader(Monitor *mon, BatchReaders *b, int *val, int *nr, int *nw)
     // Se non ci sono stampo
     printf("VALUE: %d\n", *val);
 
-    wait_sem(mon -> id_cond, S_NUM_READERS, 0);
+    wait_sem(sem, S_NUM_READERS, 0);
     // L'ultimo lettore consente allo scrittore di procedere
     if (--(*nr) == 0)
     {
-        signal_sem(mon -> id_cond, S_WRITE, 0);
+        signal_sem(sem, S_WRITE, 0);
         printf("Sono l'ultimo lettore del gruppo che rilascia il semaforo per la scrittura\n");
     }
-    signal_sem(mon -> id_cond, S_NUM_READERS, 0);
+    signal_sem(sem, S_NUM_READERS, 0);
 
-    //leave_monitor(mon);
     printf("### FINE PROCESSO LETTURA ###\n\n");
 }
 
 int main(int argc, char **argv)
 {
-	key_t key0, key1, key2, key3;
-    Monitor *mon;
+	key_t key0, key1, key2, key3, keysem;
+    int sem;
 
     /************************************
-    *   DA CONDIVIDERE CON SHM
+    *   SHARED MEMORY AREA
     */
-	int *value;
-    int *n_writers;
-    int *n_readers;
+	int *value = 0;
+    int *n_writers = 0;
+    int *n_readers = 0;
     BatchReaders* b;
     /*
     ************************************
@@ -184,6 +178,7 @@ int main(int argc, char **argv)
     if ((key1 = ftok(".", 101)) == -1) { perror("ftok"); exit(1); }
     if ((key2 = ftok(".", 102)) == -1) { perror("ftok"); exit(1); }
     if ((key3 = ftok(".", 103)) == -1) { perror("ftok"); exit(1); }
+    if ((keysem = ftok(".", 104)) == -1) { perror("ftok"); exit(1); }
 
     fprintf(stdout,"Initializing shm...\n");
 
@@ -191,25 +186,24 @@ int main(int argc, char **argv)
     get_shm(&key0, (char**)&value, sizeof(int));
     get_shm(&key1, (char**)&n_writers, sizeof(int));
     get_shm(&key2, (char**)&n_readers, sizeof(int));
-    printf("Adesso provo a creare la coda in shm...\n");
     get_shm(&key3, (char**)&b, sizeof(BatchReaders));
 
     // Reset value in case of existing shm
     *value = 0;
     *n_writers = 0;
     *n_readers = 0;
-	initBatchReaders(b);
+    initBatchReaders(b);
 
-    fprintf(stdout,"Initializing monitor...\n");
+    fprintf(stdout,"Initializing set of semaphores...\n");
 
-    // Initializing a monitor with 4 condition variable
-    mon = init_monitor(NCOND);
-    signal_sem(mon -> id_cond, S_WRITE, 0);
-    signal_sem(mon -> id_cond, S_NUM_READERS, 0);
-    signal_sem(mon -> id_cond, S_NUM_WRITERS, 0);
-    printf("S_WRITE: %d\n",semctl(mon -> id_cond, S_WRITE, GETVAL));
-    printf("S_NUM_WRITERS: %d\n",semctl(mon -> id_cond, S_NUM_WRITERS, GETVAL));
-    printf("S_NUM_READERS: %d\n",semctl(mon -> id_cond, S_NUM_READERS, GETVAL));
+    // Initializing a set of 4 semaphores with value 1
+    sem = get_sem(&keysem, NCOND, 1);
+    // The value of the fourth semaphore is set to zero
+    wait_sem(sem, S_BATCH_READERS, 0);
+    printf("S_WRITE: %d\n",semctl(sem, S_WRITE, GETVAL));
+    printf("S_NUM_WRITERS: %d\n",semctl(sem, S_NUM_WRITERS, GETVAL));
+    printf("S_NUM_READERS: %d\n",semctl(sem, S_NUM_READERS, GETVAL));
+    printf("S_BATCH_READERS: %d\n",semctl(sem, S_BATCH_READERS, GETVAL));
 
     sleep(2);
 
@@ -224,7 +218,7 @@ int main(int argc, char **argv)
             while (1) 
             {
                 sleep(1);
-                reader(mon, b, value, n_readers, n_writers);
+                reader(sem, b, value, n_readers, n_writers);
             }
         }
         else
@@ -232,19 +226,19 @@ int main(int argc, char **argv)
             while (1) 
             {
                 sleep(1);
-                reader(mon, b, value, n_readers, n_writers);
+                reader(sem, b, value, n_readers, n_writers);
             }
         }
         
     }
     else
     {
-        fprintf(stdout,"FATHER START\n");
+        fprintf(stdout,"FATHER STARTS\n");
         while (1) 
         {
             num++;
             sleep(3);
-            writer(mon, b, value, n_readers, n_writers, num);
+            writer(sem, b, value, n_readers, n_writers, num);
         }
     }
     
