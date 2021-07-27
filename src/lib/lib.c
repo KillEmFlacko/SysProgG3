ff3e4fde90ae8ac276d5ea7e6c9a611286661bc1
Armando Falanga <armando.falanga@gmail.com>
2021-07-27 18:40:58 +0200

    [lib] array maximum

--- src/lib/lib.c
+++ src/lib/lib.c
@@ -4,6 +4,8 @@
  *	This is a template code file for a static library.
  */
 
+#include <stdlib.h>
+#include <limits.h>
 #include "lib/lib.h"
 
 /*! \brief Add two integers.
@@ -14,4 +16,30 @@
  */
 int add(int a, int b){
 	return a+b;
+}
+
+/*! \brief Find max value in array
+ *
+ * \param array array in which we want to find the maximum
+ * \param len array length
+ * \param max_index pointer that will contain the index of the maximum value
+ * \return the maximum value in the array
+ */
+int array_max(int* array, int len, int *max_index)
+{
+	int i_max;
+	int max = INT_MIN;
+
+	int i;
+	for(i = 0; i < len ; i++)
+	{
+		if(array[i] > max)
+		{
+			i_max = i;
+			max = array[i];
+		}
+	}
+
+	if(max_index != NULL) *max_index = i_max;
+	return max;
 }
