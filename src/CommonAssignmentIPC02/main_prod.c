c191f84b0e851fde44695244587184e2436be4c3
JohnPAlex <a.fattore@studenti.unisa.it>
2021-07-29 12:32:52 +0200

    [IPC02] added initial comments to the producer/consumer source codes

--- src/CommonAssignmentIPC02/main_prod.c
+++ src/CommonAssignmentIPC02/main_prod.c
@@ -1,3 +1,38 @@
+/*
+ * Course: System Programming 2020/2021
+ *
+ * Lecturers:
+ * Alessia		Saggese asaggese@unisa.it
+ * Francesco	Moscato	fmoscato@unisa.it
+ *
+ * Group:
+ * D'Alessio	Simone		0622701120	s.dalessio8@studenti.unisa.it
+ * Falanga		Armando		0622701140  a.falanga13@studenti.unisa.it
+ * Fattore		Alessandro  0622701115  a.fattore@studenti.unisa.it
+ *
+ * Copyright (C) 2021 - All Rights Reserved
+ *
+ * This file is part of SysProgG3.
+ *
+ * SysProgG3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SysProgG3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SysProgG3.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+  @file main_prod.c
+  @brief Performs the producer in the producer/consumer problem synchronized with only semaphores in the case that the shared resource is an integer variable in a shared memory
+  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
